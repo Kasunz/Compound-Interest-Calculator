72aef8a9b10d5b40012d568a2ba95ab7d54892f7
Madusanka-Bandara <kasunmadhusanka44@gmail.com>
2024-10-14 20:06:20 +0530

    function to calculate compound interest

--- calculator.py
+++ calculator.py
@@ -1,6 +1,25 @@
 import tkinter as tk
 from tkinter import messagebox
 
+# Function to calculate compound interest
+def calculate():
+    try:
+        # Retrieve and validate inputs
+        principal = float(principal_entry.get())
+        rate = float(rate_entry.get()) / 100
+        time = float(time_entry.get())
+        compound = int(compound_entry.get())
+
+        # compound interest formula
+        amount = principal * (1 + rate / compound ** (compound * time))
+        interest = amount - principal
+
+        # update result label
+        result_label.config(text=f"Compound Interest = {interest : .2f}")
+
+    except ValueError:
+        messagebox.showerror("Invalid input! please enter valid numbers ")
+
 # crate main window
 root = tk.Tk()
 root.title("Compound Interest Calculater")
