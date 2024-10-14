0f62863dd3343e180ab5df6706b71a2879df34b8
Madusanka-Bandara <kasunmadhusanka44@gmail.com>
2024-10-14 20:23:59 +0530

    Initialize main tkinter window

--- calculator.py
+++ calculator.py
@@ -27,6 +27,27 @@ root.title("Compound Interest Calculater")
 # application window
 root.geometry("400x300")
 
+
+# initialize main tkinter window
+tk.Label(root, text="principle amount: ").grid(row=0, column=0, padx=10, pady=10)
+principal_entry = tk.Entry(root)
+principal_entry.grid(row=0, column=1)
+
+tk.Label(root, text="Rate of Interest (%): " ).grid(row = 0, column=0, padx=10, pady=10)
+rate_entry = tk.Entry(root)
+rate_entry.grid(row=1, column=1)
+
+tk.Label(root, text="Time (Years): ").grid(row=0, column=0, padx= 10, pady=10)
+time_entry = tk.Entry(root)
+time_entry.grid(row=2, column=1)
+
+tk.Label(root, text="Times compound (year)").grid(row=0, column=0, padx=10, pady=10)
+compound_entry = tk.Entry(root)
+compound_entry.grid(row=3, column=1)
+
+
+
+
 # main loop
 root.mainloop()
 
