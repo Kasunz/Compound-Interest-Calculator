88b0b46651d9d7cd66f6b4a1ee9859bf2a1e6583
Madusanka-Bandara <kasunmadhusanka44@gmail.com>
2024-10-14 19:57:31 +0530

    Initial setup for Tkinter application

---
+++ calculator.py
@@ -0,0 +1,13 @@
+import tkinter as tk
+from tkinter import messagebox
+
+# crate main window
+root = tk.Tk()
+root.title("Compound Interest Calculater")
+
+# application window
+root.geometry("400x300")
+
+# main loop
+root.mainloop()
+
